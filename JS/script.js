b412e003c8e3b2e31fc0a2660a6b8d667ec8edb5
André Melchior <andre.melchior@outlook.com>
2025-01-11 05:15:59 -0300

    refactor: update PHP logic for external JS call.

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,8 @@
 function clickEntrar(){
     window.location="./HTML/login.html";
 }
+
+function errorRedirect(){
+    window.alert("Dados Incorretos! Tente novamente.");
+    window.location = "../index.html";
+}
--- PHP/validacao.php
+++ PHP/validacao.php
@@ -6,19 +6,30 @@
    //comando de comparação
    //if (Se); else if (Senão Se); else (Senão)
 
-   /* e - &&
-       ou - //
+   /*  e - &&
+       ou - ||
 
        <; >; <=; >=; == (Igual); != (diferente)*/
 
-       if ($usu == "João" && $sen == "123"){
-           header("location: ../HTML/novapagina.html");
-       }else {
-           ?>
-           <script>
-               window.alert("Dados Incorretos. Tente novamente");
-               window.location="../index.html";
-            </script>
-            <?php
-       }
+    if ($usu == "João" && $sen == "123"){
+        header("location: ../HTML/novapagina.html");
+        exit;
+    }else {
+        ?>
+        <!DOCTYPE html>
+        <html lang="pt-br">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Error</title>
+                <script src="../JS/script.js"></script>
+            </head>
+            <body>
+                <script>
+                    errorRedirect();
+                </script>
+            </body>
+        </html>
+        <?php
+    }
 ?>
